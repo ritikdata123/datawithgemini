725d799438ba729f18c225f1f6f9fc4821b2c45f
ritikdata123 <119183438+ritikdata123@users.noreply.github.com>
2024-08-10 19:31:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: parseInt(process.env.PORT as string),
+    port: parseInt(process.env.PORT || "3000"),
     host: "0.0.0.0",
     cors: true
   }
