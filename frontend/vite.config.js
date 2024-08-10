40c812913b4839d0fd1a92b8f01d71dfe23a2816
ritikdata123 <119183438+ritikdata123@users.noreply.github.com>
2024-08-10 19:28:24 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    port: parseInt(process.env.PORT as string),
+    host: "0.0.0.0",
+    cors: true
+  }
 })
